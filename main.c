83c51b70a9124d002b0adbdfa091ced7c8dfce83
ale <adad>
2019-11-01 19:51:09 +0300

    add

--- main.c
+++ main.c
@@ -6,26 +6,63 @@
 /*   By: alexzudin <alexzudin@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/13 08:15:19 by aguiller          #+#    #+#             */
-/*   Updated: 2019/10/24 20:47:49 by alexzudin        ###   ########.fr       */
+/*   Updated: 2019/11/01 19:50:36 by alexzudin        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char **rd(int fd)
+
+int checkline(char *line, int *strok, int *reshotki)
 {
-	char	**massive;
+	if (line[0])
+		(*strok)++;
+	else 
+	{	
+		if (*strok != 4)
+			return (0);
+		*strok = 0;
+		if (*reshotki != 4)
+			return (0);
+		*reshotki = 0;
+	}
+	return (1);
+}
+int lastcheck(int i, char **line)
+{
+	if (i != 4 && (*line)[0])
+		return (0);
+	ft_strdel(line);
+	return (1);
+}
+int first_check(int fd)
+{
+	int		reshotki;
 	char	*line;
+	int		strok;
+	int		i;
 
-	massive = NULL;
 	line = NULL;
+	reshotki = 0;
+	strok = 0;
 	while (get_next_line(fd, &line) > 0)
 	{
-
+		i = 0;
+		if (checkline(line, &strok, &reshotki) == 0)
+			return (0);
+		while (line[i])
+		{
+			if (line[i] != '#' && line[i] != '.')
+				return (0);
+			if(line[i] == '#')
+				reshotki++;
+			i++;
+		}
+		if (lastcheck(i, &line) == 0)
+			return (0);
 	}
-
-	return (0);
-
+	return (1);
 }
 
 int checkarg(int argc, char **argv)
@@ -60,7 +97,10 @@ int main(int argc, char **argv)
 	massive = NULL;
 	if ((fd = checkarg(argc,argv)) == 0)
 		return (0);
-	
-
+	if(first_check(fd) == 0)
+	{
+		ft_putendl("error");
+		return (0);
+	}
 	return (0);
 }
